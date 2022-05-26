a424ca5bf358832221b29094d261ecebbf2e90a9
wtlin1228 <wtlin1228@gmail.com>
2022-05-26 23:18:00 +0800

    add lc.2134

---
+++ leetcode/2134-minimum-swaps-to-group-all-1s-together-ii/solution-1-sliding-window.js
@@ -0,0 +1,47 @@
+// Sliding Window Approach:
+//   1. calculate the total 1's -> S
+//   2. create a window with size S
+//   3. slide the window form left to right for n steps,
+//      and trace the minium number of holes in the window.
+//   4. the minium holes is our answer
+//
+// Complexity Analysis
+// - Time Complexity: O(n), where n = data.length
+// - Space Complexity: O(1)
+
+/**
+ * @param {number[]} data
+ * @return {number}
+ */
+var minSwaps = function (data) {
+  let totalOnes = 0
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] === 1) {
+      totalOnes += 1
+    }
+  }
+
+  let holes = 0
+  for (let i = 0; i < totalOnes; i++) {
+    if (data[i] === 0) {
+      holes += 1
+    }
+  }
+
+  let min = holes
+  for (let i = totalOnes; i < totalOnes + data.length; i++) {
+    if (data[i % data.length] === 0) {
+      holes += 1
+    }
+
+    if (data[(i - totalOnes) % data.length] === 0) {
+      holes -= 1
+    }
+
+    if (holes < min) {
+      min = holes
+    }
+  }
+
+  return min
+}
